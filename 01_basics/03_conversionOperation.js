beaefe6c8c3332c772535e5eccce7cc330a67c4b
Rahull13 <43403968+Rahull13@users.noreply.github.com>
2024-03-21 18:29:02 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "rahul"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33asd" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "rahu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "rahu" => true
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
