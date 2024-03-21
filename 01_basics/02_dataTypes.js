f2950a3a7456eb867ea3a99e14390aa3a9be5275
Rahull13 <43403968+Rahull13@users.noreply.github.com>
2024-03-21 17:52:36 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert('test') // We are using nodejs, not browser
+
+let name = 'rahul'
+let age = 23
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
