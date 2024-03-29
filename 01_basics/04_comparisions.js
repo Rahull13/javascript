6afe5c4e06c76478904dc4a6eb24d470d97e46bf
Rahull13 <43403968+Rahull13@users.noreply.github.com>
2024-03-29 16:05:42 +0000

    comparision

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // Output False
+console.log(null == 0); // Output False
+console.log(null >= 0); // Output True
+
+console.log(undefined == 0); // Output False
+console.log(undefined >= 0); // Output False
+console.log(undefined > 0); // Output False
+
+// === (Data check with datatype)
+
+console.log("2" === 2);  // Output False
