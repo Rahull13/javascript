ed5dbaec8ac93c745371ac90f043234bf49779e4
Rahull13 <43403968+Rahull13@users.noreply.github.com>
2024-05-06 18:14:55 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March"
+
+switch (month) {
+    case "Jan":
+        console.log('Jan');
+        break;
+    case "Feb":
+        console.log('Feb');
+        break;
+    case "March":
+        console.log('March');
+        break;
+    case "Apr":
+        console.log('Apr');
+        break;
+
+    default:
+        console.log('else');
+        break;
+}
